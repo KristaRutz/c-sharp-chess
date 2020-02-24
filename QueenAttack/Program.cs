b555a80c0de85602cfa4dcf2c9babe6f8ff5c8db
Andrew Philpott and Steven Fleming <steven@>
2020-02-24 11:05:33 -0800

    allow user to compare a queens location to another pieces location and return whether it can be attacked

--- QueenAttack/Program.cs
+++ QueenAttack/Program.cs
@@ -1,8 +1,44 @@
 using System;
+using QueenAttack;
 class Program
 {
+  
+  static void PrintBoard()
+  {
+    Console.WriteLine("CURRENT BOARD");
+    for (int rows = 0; rows < 8; rows++)
+    {
+      Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+    }
+    
+  }
   static void Main()
   {
-   
+    PrintBoard();
+  //  Console.WriteLine("Determine if a queen can attack another chess piece.");
+  //  Console.WriteLine("Enter coordinates for the queen.");
+  //  Console.WriteLine("Enter x coordinate.");
+  //  string queenXCoord = Console.ReadLine();
+  //  int queenX = int.Parse(queenXCoord);
+  //  Console.WriteLine("Enter y coordinate.");
+  //  string queenYCoord = Console.ReadLine();
+  //  int queenY = int.Parse(queenYCoord);
+  //  Queen q = new Queen(queenX, queenY);
+  //  Console.WriteLine("Enter coordinates for the other chess piece.");
+  //  Console.WriteLine("Enter x coordinate.");
+  //  string pieceXCoord = Console.ReadLine();
+  //  int pieceX = int.Parse(pieceXCoord);
+  //  Console.WriteLine("Enter y coordinate.");
+  //  string pieceYCoord = Console.ReadLine();
+  //  int pieceY = int.Parse(pieceYCoord);
+  //  bool attackable = q.CheckAll(pieceX, pieceY);
+  //  if(attackable) {
+  //    Console.WriteLine("The queen can attack the other piece.");
+  //    Console.WriteLine("Piece out.");
+  //  } else {
+  //    Console.WriteLine("The queen can't attack the other piece.");
+  //    Console.WriteLine("Piece out.");
+  //  }
+  //  Main();
   }
 }
--- QueenAttack/Queen.cs
+++ QueenAttack/Queen.cs
@@ -3,7 +3,42 @@ using System;
 namespace QueenAttack {
 public class Queen {
   
-  public Queen() {
+  public int XCoord {get; set;}
+  public int YCoord {get; set;}
+  public Queen(int xCoord, int yCoord) {
+      XCoord = xCoord;
+      YCoord = yCoord;
+  }
+
+  public bool CheckAll(int x, int y) {
+    if(CheckSameColumn(x, y) || CheckSameRow(x, y) || CheckDiagonal(x,y)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public bool CheckSameColumn(int x, int y) {
+    if(XCoord == x) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public bool CheckSameRow(int x, int y) {
+    if(YCoord == y) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public bool CheckDiagonal(int x, int y) {
+    int rise = (y - YCoord);
+    int run = (x - XCoord);
+    if(Math.Abs(rise/run) == 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 }
